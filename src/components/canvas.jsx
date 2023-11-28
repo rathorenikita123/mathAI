8ea98b5beb712055c67c16603ab72ee276e7fad1
RNikita123t <9752rathorenikita@gmail.com>
2023-11-28 19:47:02 +0530

    add functionality

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -5,11 +5,15 @@ const Canvas = () => {
   const canvasRef = useRef(null);
   const [points, setPoints] = useState({});
   const [selectedPoint, setSelectedPoint] = useState(null);
+  const [drawMode, setDrawMode] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
 
     const handleStart = (x, y) => {
+      if (!drawMode) {
+        return; // If drawMode is disabled, do not add new points
+      }
       const newPoints = { ...points };
 
       // Check proximity to existing points
@@ -31,11 +35,12 @@ const Canvas = () => {
     };
 
     const handleMove = (x, y) => {
-      if (selectedPoint) {
-        const newPoints = { ...points };
-        newPoints[selectedPoint] = { x, y };
-        setPoints(newPoints);
+      if (!drawMode || !selectedPoint) {
+        return; // If drawMode is disabled or no point is selected, do not proceed with movement
       }
+      const newPoints = { ...points };
+      newPoints[selectedPoint] = { x, y };
+      setPoints(newPoints);
     };
 
     const handleEnd = () => {
@@ -73,10 +78,14 @@ const Canvas = () => {
       canvas.removeEventListener("mousemove", handleCanvasMove);
       canvas.removeEventListener("mouseup", handleCanvasEnd);
     };
-  }, [points, selectedPoint]);
+  }, [points, selectedPoint, drawMode]);
 
   const handleClearLines = () => {
     setPoints({});
+  };
+
+  const handleToggleDrawMode = () => {
+    setDrawMode((prevMode) => !prevMode);
   };
 
   useEffect(() => {
@@ -109,81 +118,97 @@ const Canvas = () => {
 
     // Draw line segments based on the points and calculate length
     const pointKeys = Object.keys(points);
-    let totalLength = 0;
     for (let i = 0; i < pointKeys.length - 1; i++) {
       const startPoint = points[pointKeys[i]];
       const endPoint = points[pointKeys[i + 1]];
 
-      context.beginPath();
-      context.moveTo(startPoint.x, startPoint.y);
-      context.lineTo(endPoint.x, endPoint.y);
-      context.strokeStyle = "black";
-      context.stroke();
+      if (drawMode) {
+        // Draw only if drawMode is true
+        context.beginPath();
+        context.moveTo(startPoint.x, startPoint.y);
+        context.lineTo(endPoint.x, endPoint.y);
+        context.strokeStyle = "black";
+        context.stroke();
+      }
 
-      // Calculate and displaylength in cm
+      // Calculate and display length in cm
       const segmentLength =
         Math.sqrt(
           (endPoint.x - startPoint.x) ** 2 + (endPoint.y - startPoint.y) ** 2
         ) / 20;
-      totalLength += segmentLength;
       const midX = (startPoint.x + endPoint.x) / 2;
       const midY = (startPoint.y + endPoint.y) / 2;
       context.fillStyle = "black";
       context.fillText(`${segmentLength.toFixed(2)} cm`, midX, midY - 5);
     }
 
-    // Display total length in cm
-    context.fillStyle = "black";
-    context.fillText(
-      `Total Length: ${totalLength.toFixed(2)} cm`,
-      canvas.width - 120,
-      20
-    );
+    const scalingFactor = 20;
 
-    // Calculate and display angles in degrees with arcs between line segments
-    for (let i = 1; i < pointKeys.length - 1; i++) {
-      const pointA = points[pointKeys[i - 1]];
-      const pointB = points[pointKeys[i]];
-      const pointC = points[pointKeys[i + 1]];
+    // Calculate angles and display
+    if (pointKeys.length === 3) {
+      const pointA = points[pointKeys[0]];
+      const pointB = points[pointKeys[1]];
+      const pointC = points[pointKeys[2]];
 
-      const angle = calculateAngle(pointA, pointB, pointC);
+      const sideAB = calculateDistance(pointA, pointB) / scalingFactor;
+      const sideBC = calculateDistance(pointB, pointC) / scalingFactor;
+      const sideCA = calculateDistance(pointC, pointA) / scalingFactor;
 
-      // Draw an arc between the line segments for the angle
-      context.beginPath();
-      context.arc(
-        pointB.x,
-        pointB.y,
-        30,
-        Math.atan2(pointA.y - pointB.y, pointA.x - pointB.x),
-        Math.atan2(pointC.y - pointB.y, pointC.x - pointB.x)
-      );
-      context.strokeStyle = "blue";
-      context.stroke();
+      const angleA = calculateAngle(sideBC, sideCA, sideAB);
+      const angleB = calculateAngle(sideCA, sideAB, sideBC);
+      const angleC = calculateAngle(sideAB, sideBC, sideCA);
 
-      // Label the angle near the arc
+      // Display angles
       context.fillStyle = "blue";
-      context.fillText(`${angle.toFixed(2)}°`, pointB.x, pointB.y);
+      context.fillText(
+        `Angle A: ${angleA.toFixed(2)}°`,
+        pointA.x,
+        pointA.y - 20
+      );
+      context.fillText(
+        `Angle B: ${angleB.toFixed(2)}°`,
+        pointB.x,
+        pointB.y - 20
+      );
+      context.fillText(
+        `Angle C: ${angleC.toFixed(2)}°`,
+        pointC.x,
+        pointC.y - 20
+      );
+
+      // Display lengths
+      const midX_AC = (pointA.x + pointC.x) / 2.5;
+      const midY_AC = (pointA.y + pointC.y) / 2;
+      context.fillStyle = "black";
+      context.fillText(`${sideCA.toFixed(2)} cm`, midX_AC, midY_AC);
     }
-  }, [points]);
 
-  const calculateAngle = (pointA, pointB, pointC) => {
-    const vector1 = { x: pointA.x - pointB.x, y: pointA.y - pointB.y };
-    const vector2 = { x: pointC.x - pointB.x, y: pointC.y - pointB.y };
+    // Draw the triangle by connecting the first and last points
+    if (pointKeys.length >= 3 && drawMode) {
+      const firstPoint = points[pointKeys[0]];
+      const lastPoint = points[pointKeys[pointKeys.length - 1]];
 
-    const dotProduct = vector1.x * vector2.x + vector1.y * vector2.y;
-    const magnitude1 = Math.sqrt(vector1.x ** 2 + vector1.y ** 2);
-    const magnitude2 = Math.sqrt(vector2.x ** 2 + vector2.y ** 2);
+      context.beginPath();
+      context.moveTo(firstPoint.x, firstPoint.y);
+      context.lineTo(lastPoint.x, lastPoint.y);
+      context.strokeStyle = "black";
+      context.stroke();
+    }
+  }, [points, drawMode]);
 
-    const cosTheta = dotProduct / (magnitude1 * magnitude2);
-    const angleRad = Math.acos(cosTheta);
+  const calculateDistance = (point1, point2) => {
+    return Math.sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2);
+  };
 
-    return angleRad * (180 / Math.PI);
+  const calculateAngle = (a, b, c) => {
+    const radianAngle = Math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b));
+    return (radianAngle * 180) / Math.PI;
   };
 
   return (
     <div className="p-4">
       <h1 className="text-3xl font-semibold p-4 text-center">
-        <span className="text-green-700">Welcome,</span> lets learn to draw
+        <span className="text-green-700">Welcome,</span> let's learn to draw
         figures
       </h1>
       <div className="flex justify-center">
@@ -195,12 +220,26 @@ const Canvas = () => {
             height={800}
             className="bg-white"
           ></canvas>
-          <button
-            onClick={handleClearLines}
-            className="p-2 bg-red-600 rounded-md text-white"
-          >
-            Clear Lines
-          </button>
+          <div className="flex justify-evenly">
+            <button
+              onClick={handleToggleDrawMode}
+              className={`p-2 ${
+                drawMode ? "bg-blue-600" : "bg-gray-400"
+              } rounded-md text-white`}
+            >
+              {drawMode ? (
+                <img src="./images/line.png" alt="line" width={100} />
+              ) : (
+                <img src="./images/line.png" alt="line" width={100} />
+              )}
+            </button>
+            <button
+              onClick={handleClearLines}
+              className="p-2 bg-red-600 rounded-md text-white"
+            >
+              Clear Lines
+            </button>
+          </div>
         </div>
       </div>
     </div>
